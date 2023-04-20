0d16aac300b8761c433d2eb5857272ff043b65c3
DavidGalvis <dgalvis@tiresia.co>
2023-04-20 00:05:14 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+const scanner = new Html5QrcodeScanner('reader', { 
+    // Inicializo el scanner
+    qrbox: {
+        width: 250,
+        height: 250,
+    },  // Defino dimenciones del qrbox 
+    fps: 60, // Frames 
+});
+
+
+scanner.render(success, error);
+// Retornos
+
+function success(result) {
+    buscarInfo(result);
+    scanner.clear();
+    //document.getElementById('reader').remove();
+}
+
+const buscarInfo = async  (result) => {
+
+    const fecha = new Date();
+    const id2 = `${fecha.getFullYear()}-${(fecha.getMonth() + 1).toString().padStart(2, '0')}-${fecha.getDate().toString().padStart(2, '0')} ${fecha.getHours().toString().padStart(2, '0')}:${fecha.getMinutes().toString().padStart(2, '0')}:${fecha.getSeconds().toString().padStart(2, '0')}`;
+    let ficho = parseInt(result, 10);
+    const id=ficho;
+    const response = await fetch("http://localhost:4000/api/kid/scanner/"+id+"/"+id2);
+    const registro = await response.json();
+    const Dk=registro[0].id;
+    registro.forEach((reg) => {
+        document.getElementById('info').innerHTML = `
+        <h2>Escaneo Exitoso!</h2>
+        <p>El numero del ficho es: ${reg.ficho} <br></p>
+        <p>El nombre del niño es: ${reg.nombres} <br></p>
+        <p>Edad: ${reg.edad} <br></p>
+        <p>Informacion del acudiente #1: <br> ${reg.nombrep1} <br> ${reg.celular1} <br></p>
+        <p>Informacion del acudiente #2: <br> ${reg.nombrep2} <br> ${reg.celular2} <br></p>
+        <p>Fecha y hora del registro: ${reg.fechacreacion}</p><br>
+        `;
+    });
+    await actulizarStatus(Dk);  
+}
+
+const actulizarStatus = async  (Dk) => {
+    const id = Dk;
+    const datos = {
+        estado: 'LEIDO'
+    };
+    const response =  fetch("http://localhost:4000/api/kid/scanner/actu/"+id,{
+        method:'PUT',
+        headers:{'Content-Type': 'application/json'},
+        body:JSON.stringify(datos)
+    }).then(response => response.json())
+    .then(data => {
+    if (data.status == 'ok') {
+        alert('La solicitud se realizó con éxito. Respuesta: ' + data.message);
+    } else {
+        alert('Se produjo un error en la solicitud. Mensaje de error: ' + data.error);
+    }
+    })
+    .catch(error => console.error('Error:', error));         
+};
+
+
+function error(err) {
+    if (err != 'QR code parse error, error = D: No MultiFormat Readers were able to detect the code.') {
+        console.error(err); 
+    }
+}
