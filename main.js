b8649a1628dc0e7eea337a4dfa53485516adcbf5
David Galvis <santiagogalvis771@gmail.com>
2026-05-03 00:16:57 -0700

    ajustes

--- main.js
+++ main.js
@@ -18,34 +18,44 @@ function success(result) {
 }
 
 const buscarInfo = async  (result) => {
-
-    const fecha = new Date();
-    const id2 = `${fecha.getFullYear()}-${(fecha.getMonth() + 1).toString().padStart(2, '0')}-${fecha.getDate().toString().padStart(2, '0')} ${fecha.getHours().toString().padStart(2, '0')}:${fecha.getMinutes().toString().padStart(2, '0')}:${fecha.getSeconds().toString().padStart(2, '0')}`;
+    console.log("result",result);
+    
     let ficho = parseInt(result, 10);
     const id=ficho;
-    //console.log(id)
-    //console.log(id2)
-    const response = await fetch("https://rocakids.com.co:4014/api/kid/scanner/"+id+"/"+id2); // PRODUCCION
+    console.log("id",id)
+    
+    const response = await fetch("https://rocakids.com.co/api/kid/scanner/"+id); // PRODUCCION
     //const response = await fetch("https://localhost:4014/api/kid/scanner/"+id+"/"+id2); // PRUEBAS
     const registro = await response.json();
-    const Dk=registro[0].id;
-    registro.forEach((reg) => {
+    console.log("registro",registro)
+    if(registro && registro!=''){
+        const Dk=registro[0].id;
+        registro.forEach((reg) => {
+            document.getElementById('info').innerHTML = `
+            <input id="idReg" style="display:none;">
+            <h2>Escaneo Exitoso!</h2>
+            <p>El numero del ficho es: ${reg.ficho} <br></p>
+            <p>El nombre del niño es: ${reg.nombres} <br></p>
+            <p>Edad: ${reg.edad} <br></p>
+            <p>Informacion del acudiente #1: <br> ${reg.nombrep1} <br> ${reg.celular1} <br></p>
+            <p>Informacion del acudiente #2: <br> ${reg.nombrep2} <br> ${reg.celular2} <br></p>
+            <p>Fecha y hora del registro: ${reg.fechacreacion} <br></p>
+            <button id="btnRecargar" class="btn btn-primary" onclick="reload()">Escanear de nuevo</button>
+            <button id="btnEstado" class="btn btn-warning" onclick="actulizarStatusACTIVO()">ACTIVAR CODIGO</button>
+            `;
+        });
+        const IdRegInput = document.querySelector('#idReg');
+        IdRegInput.value = Dk;
+        await actulizarStatus(Dk);  
+    }else{
         document.getElementById('info').innerHTML = `
-        <input id="idReg" style="display:none;">
-        <h2>Escaneo Exitoso!</h2>
-        <p>El numero del ficho es: ${reg.ficho} <br></p>
-        <p>El nombre del niño es: ${reg.nombres} <br></p>
-        <p>Edad: ${reg.edad} <br></p>
-        <p>Informacion del acudiente #1: <br> ${reg.nombrep1} <br> ${reg.celular1} <br></p>
-        <p>Informacion del acudiente #2: <br> ${reg.nombrep2} <br> ${reg.celular2} <br></p>
-        <p>Fecha y hora del registro: ${reg.fechacreacion} <br></p>
-        <button id="btnRecargar" class="btn btn-primary" onclick="reload()">Escanear de nuevo</button>
-        <button id="btnEstado" class="btn btn-warning" onclick="actulizarStatusACTIVO()">ACTIVAR CODIGO</button>
-        `;
-    });
-    const IdRegInput = document.querySelector('#idReg');
-    IdRegInput.value = Dk;
-    await actulizarStatus(Dk);  
+            <input id="idReg" style="display:none;">
+            <h2>Opss!</h2>
+            <p>No se encontraron registros con el ficho: ${id}.<br></p>
+            <p>O ya se encuentra en estado LEIDO <br><br></p>
+            <button id="btnRecargar" class="btn btn-primary" onclick="reload()">Escanear de nuevo</button>
+            `;
+    }
 }
 
 const actulizarStatus = async  (Dk) => {
@@ -54,7 +64,7 @@ const actulizarStatus = async  (Dk) => {
         estado: 'LEIDO'
     };
     //const response =  fetch("https://localhost:4014/api/kid/scanner/actu/"+id,{ // PRUEBAS
-    const response =  fetch("https://rocakids.com.co:4014/api/kid/scanner/actu/"+id,{
+    const response =  fetch("https://rocakids.com.co/api/kid/scanner/actu/"+id,{
         method:'PUT',
         headers:{'Content-Type': 'application/json'},
         body:JSON.stringify(datos)
@@ -78,7 +88,7 @@ const actulizarStatusACTIVO = async  () => {
         estado: 'ACTIVO'
     };
     //const response =  fetch("https://localhost:4014/api/kid/scanner/actu/"+id,{ // PRUEBAS
-    const response =  fetch("https://rocakids.com.co:4014/api/kid/scanner/actu/"+id,{
+    const response =  fetch("https://rocakids.com.co/api/kid/scanner/actu/"+id,{
         method:'PUT',
         headers:{'Content-Type': 'application/json'},
         body:JSON.stringify(datos)
